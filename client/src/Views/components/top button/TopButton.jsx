0681a1fa8b65cc7f77531d1894d05c6839c862c0
Vaghinak Papoyan <50072682+Anonimus1423@users.noreply.github.com>
2023-02-13 20:22:53 +0300

    sadf

--- client/src/Views/components/top button/TopButton.jsx
+++ client/src/Views/components/top button/TopButton.jsx
@@ -4,17 +4,19 @@ import { ReactComponent as TopButton1} from "../../images/icons/Top Button.svg"
 function TopButton() 
 {
     let scrollY = useState(window.screenTop)
-    let show = useState(false);
+    let show = false;
     console.log(scrollY, show);
-    if(scrollY > 3000)
+    useEffect(() => 
     {
-        scrollY.setState(true)
-    }
-    else
+        if(scrollY > 3000)
+        {
+            show = true
+        }
+    }, [scrollY])
+    window.addEventListener("scroll", () => 
     {
-        scrollY.setState(false)
-    }
-    
+        scrollY.setState(() => window.screenTop)
+    })
     return ( 
         show
         ?
