729b0cda3f3c2833b8d2f153e5920416c68a0a04
Vaghinak Papoyan <ararat.armenia.02@mail.ru>
2023-03-11 13:58:35 +0300

    adaptive

--- client/src/Views/components/header/Header.jsx
+++ client/src/Views/components/header/Header.jsx
@@ -5,8 +5,9 @@ import "./style/index.scss";
 import { useSelector } from "react-redux";
 import ProfileIcon from "../../images/icons/Profile.svg";
 import * as appSelectors from "../../../redux/app/selectors";
+import PopUp from "../popup/PopUp";
 
-function Header({ buttons, setSidebarOpened, sidebarOpened }) {
+function Header({ buttons }) {
   const app = useSelector(appSelectors.appSelector);
   let authorized = false;
   const role = useSelector(appSelectors.roleSelector);
@@ -28,6 +29,20 @@ function Header({ buttons, setSidebarOpened, sidebarOpened }) {
           </Link>
           <div className="profile">
             <h6>A2</h6>
+            <PopUp
+              title="Right ansrer is 3"
+              position={{
+                top: "100%",
+                left: "100%",
+                transform:
+                  "translateX(-100%) translateX(-100px) translateY(15px)",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+              accumsan et viverra justo commodo. Proin sodales pulvinar sic
+              tempor. Sociis natoque penatibus et magnis dis parturient?
+            </PopUp>
             <div className="profile-icon">
               <img src={ProfileIcon} alt="profile" />
             </div>
--- client/src/Views/main/Result/Index.jsx
+++ client/src/Views/main/Result/Index.jsx
@@ -6,6 +6,7 @@ import OkIcon from "../../images/icons/Ok.svg";
 import InstagramIcon from "../../images/icons/Instagram.svg";
 import FacebookIcon from "../../images/icons/Facebook.svg";
 import TwitterIcon from "../../images/icons/Twitter.svg";
+import Footer from "../../components/footer/Footer.jsx";
 
 function Result() {
   return (
@@ -18,69 +19,74 @@ function Result() {
           secondLink: "/registration",
         }}
       />
-      <h2 className="secondPage">Ստուգեք Ձեր անգլերենի մակարդակը</h2>
-      <p>
-        Շնորհավորում ենք դուք անցել եք թեստը <br /> կիսվեք Ձեր հաջողությամբ
-        սոցիալական կայքերում
-      </p>
-      <div className="social-icons">
-        <div className="social-icon">
-          <img src={VkIcon} alt="vk" />
-        </div>
-        <div className="social-icon">
-          <img src={InstagramIcon} alt="instagram" />
-        </div>
-        <div className="social-icon">
-          <img src={TwitterIcon} alt="twitter" />
-        </div>
-        <div className="social-icon">
-          <img src={FacebookIcon} alt="facebook" />
-        </div>
-        <div className="social-icon">
-          <img src={OkIcon} alt="ok" />
-        </div>
-      </div>
-      <div className="table">
-        <div className="row">
-          <div className="left">
-            <p>Բոլոր հարցերը</p>
-          </div>
-          <div className="right">
-            <p>48</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="left">
-            <p>Ջիշտ պատասխաններ</p>
-          </div>
-          <div className="right">
-            <p>32</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="left">
-            <p>Սխալպատասխաններ</p>
-          </div>
-          <div className="right">
-            <p>16</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="left">
-            <p>Սխալպատասխաններ</p>
-          </div>
-          <div className="right">
-            <p>16</p>
-          </div>
-        </div>
-      </div>
-      <div className="buttons">
-        <MainButton color="transparent-yellow-bordered">Կրկնել</MainButton>
-        <MainButton color="yellow">Բարելավե՛ք ձեր անգլերենը:</MainButton>
-        <p className="s">
-          Անցիր մեր դասընթացը հատուկ C1 <br /> մակարդակի համար
+      <div className="right-container">
+        <h2 className="secondPage">Ստուգեք Ձեր անգլերենի մակարդակը</h2>
+        <p>
+          Շնորհավորում ենք դուք անցել եք թեստը <br /> կիսվեք Ձեր հաջողությամբ
+          սոցիալական կայքերում
         </p>
+        <div className="social-icons">
+          <div className="social-icon">
+            <img src={VkIcon} alt="vk" />
+          </div>
+          <div className="social-icon">
+            <img src={InstagramIcon} alt="instagram" />
+          </div>
+          <div className="social-icon">
+            <img src={TwitterIcon} alt="twitter" />
+          </div>
+          <div className="social-icon">
+            <img src={FacebookIcon} alt="facebook" />
+          </div>
+          <div className="social-icon">
+            <img src={OkIcon} alt="ok" />
+          </div>
+        </div>
+        <div className="table">
+          <div className="row">
+            <div className="left">
+              <p>Բոլոր հարցերը</p>
+            </div>
+            <div className="right">
+              <p>48</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="left">
+              <p>Ջիշտ պատասխաններ</p>
+            </div>
+            <div className="right">
+              <p>32</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="left">
+              <p>Սխալպատասխաններ</p>
+            </div>
+            <div className="right">
+              <p>16</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="left">
+              <p>Սխալպատասխաններ</p>
+            </div>
+            <div className="right">
+              <p>16</p>
+            </div>
+          </div>
+        </div>
+        <div className="buttons">
+          <MainButton color="transparent-yellow-bordered">Կրկնել</MainButton>
+          <div className="right">
+            <MainButton color="yellow">Բարելավե՛ք ձեր անգլերենը:</MainButton>
+            <p className="s">
+              Անցիր մեր դասընթացը հատուկ C1 <br /> մակարդակի համար
+            </p>
+          </div>
+        </div>
       </div>
+      <Footer fixed />
     </div>
   );
 }
