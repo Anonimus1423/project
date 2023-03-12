2e19e9b50e3be409aceab6176c94c618cb3f5e06
rudmargaryan <rudikhayk2017@gmail.com>
2023-03-12 08:53:17 +0400

    default test get without logining

--- back/routes/router.js
+++ back/routes/router.js
@@ -2,10 +2,13 @@ import express from "express";
 import tokenValidator from "../utils/tokenValidator.js";
 import CoursesRouter from "./Courses/router.js";
 import ApiRoutes from "./Api/router.js";
+import { getDefaultTest } from "./Admin/services.js";
 
 const router = express.Router();
 
 router.use("/courses", CoursesRouter);
+
+router.get("/test", getDefaultTest);
 
 router.use(tokenValidator);
 
