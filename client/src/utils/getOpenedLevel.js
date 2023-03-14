a0e0c82639bb3f79dbb8fda88c2410ba71d2d99c
rudmargaryan <rudikhayk2017@gmail.com>
2023-03-14 22:41:52 +0400

    changes

--- client/src/utils/getOpenedLevel.js
+++ client/src/utils/getOpenedLevel.js
@@ -1,14 +1,13 @@
-export default function getOpenedLevel(level){
-    let openedLevel = [level];
-    if(level === "A2")
-        openedLevel.push("A1")
-    if(level === "B1")
-        openedLevel.push("A1").push("A2")
-    if(level === "B2")
-        openedLevel.push("A1").push("A2").push("B1")
-    if(level === "C1")
-        openedLevel.push("A1").push("A2").push("B1").push("B2")
-    if(level === "C2")
-        openedLevel.push("A1").push("A2").push("B1").push("B2").push("C1")
-    return openedLevel
+export default function getOpenedLevel(level) {
+  let openedLevel = [level];
+  console.log(openedLevel);
+  if (level === "A2") {
+    openedLevel = [...openedLevel, "A1"];
+  }
+  if (level === "B1") openedLevel = [...openedLevel, "A1", "A2"];
+  if (level === "B2") openedLevel = [...openedLevel, "A1", "A2", "B1"];
+  if (level === "C1") openedLevel = [...openedLevel, "A1", "A2", "B1", "B2"];
+  if (level === "C2")
+    openedLevel = [...openedLevel, "A1", "A2", "B1", "B2", "C2"];
+  return openedLevel;
 }
