b4894c997660f52e4d18bbba59a6ccf06c751d7d
root <root@1348257-cj56605.tw1.ru>
2023-03-25 08:03:48 +0300

    ssl server

--- back/index.js
+++ back/index.js
@@ -4,7 +4,16 @@ import { connect } from "mongoose";
 import tokenValidator from "./utils/tokenValidator.js";
 import UserRouter from "./routes/User/router.js";
 import Router from "./routes/router.js";
+import cors from 'cors'
+import https from 'https'
+import path from "path";
+import fs from 'fs'
 import AdminRouter from "./routes/Admin/router.js";
+import { fileURLToPath } from 'url';
+
+// Dirname on ES6 Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //configs
 const port = config.get("server.port");
@@ -14,15 +23,31 @@ const bd = config.get("databse.mongoURL");
 const app = express();
 
 // middlewares
+app.use(cors())
 app.use(express.json());
 
 // ROUTES
 
+app.get('/',(req,res)=>{
+  return res.status(200)
+})
 app.use("/user", UserRouter);
 app.use("/api", Router);
 app.use("/admin", AdminRouter);
 
-app.listen(port, () => {
+
+// ssl Server
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+  cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
+},app)
+
+sslServer.listen(port,()=>{
   connect(bd).then(() => console.log("Mongoo connected!!"));
-  console.log(`Server has been started on port ${port}`);
-});
+  console.log('started ssl server')
+})
+
+// app.listen(port, () => {
+//   connect(bd).then(() => console.log("Mongoo connected!!"));
+//   console.log(`Server has been started on port ${port}`);
+// });
